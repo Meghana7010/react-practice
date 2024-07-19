a4acb9a8127c2b6051c89042eb5e0a5bd81e6d45
unknown <meghanamaheshwari1@gmail.com>
2024-07-19 17:38:58 +0530

    react-buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleClick = (type) => {
+    if(type === 'inc')setCount(count+1);
+     else setCount(count-1);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{count}</p>
+     <button onClick = {()=>handleClick('inc')}>Increment</button>
+     <button onClick = {()=>handleClick('dec')}>Decrement</button>
+
       </header>
     </div>
   );
